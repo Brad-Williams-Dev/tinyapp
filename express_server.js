ba1e784e4213033976aee8edfe53050890c3fc8b
Brad Williams <bradley_williams2003@hotmail.com>
2022-11-28 14:42:29 -0400

    Add urls_new template page, added GET request to express_server, made random string function

--- express_server.js
+++ express_server.js
@@ -2,12 +2,37 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+const generateRandomString = () => {
+  let result = '';
+  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return result;
+};
+
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+// ------------------------- MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+
+
+//--------------------------GET REQUESTS
+
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -30,6 +55,14 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body> Hello <b>World</b></body></html>\n");
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+
+// -------------------------- LISTENER
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
