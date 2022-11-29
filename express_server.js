b37f62efc0dd748aaef6c73ba1d3b8f5908e79ca
Brad Williams <bradley_williams2003@hotmail.com>
2022-11-29 15:29:54 -0400

    Added cookies, added login/logout feature on navbar with, everything tested working

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 
 // --------------------- REQUIREMENTS
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -27,26 +28,31 @@ const urlDatabase = {
 // ------------------------- MIDDLEWARE
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 //--------------------------GET REQUESTS
 
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
+
   res.redirect(longURL);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -69,6 +75,18 @@ app.post("/urls/:id/edit", (req, res) => {
   const longURL = req.body.updatedLongURL;
   urlDatabase[shortURL] = longURL;
   res.redirect('/urls');
+});
+
+// -------- LOGIN BUTTON POST REQUEST -------
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+// --------- LOGOUT BUTTON POST REQUEST -----
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
 });
 
 app.post("/urls/:id/update", (req, res) => {
