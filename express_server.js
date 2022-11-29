717982d6257b2fe143fbfb96d0423edbc5d166b6
Brad Williams <bradley_williams2003@hotmail.com>
2022-11-29 13:49:35 -0400

    added "finished" button to urls/:id page that simply returns to the homepage

--- express_server.js
+++ express_server.js
@@ -63,9 +63,11 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect('/urls');
 });
 
-// -------EDIT BUTTON POST REQUEST ------
-app.get("/urls/:id/update", (req, res) => {
-
+// -------EDIT BUTTON POST REQUEST ---------
+app.post("/urls/:id/edit", (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = req.body.updatedLongURL;
+  urlDatabase[shortURL] = longURL;
   res.redirect('/urls');
 });
 
